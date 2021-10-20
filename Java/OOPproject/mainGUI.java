04226b8286dfdcd28c6f7923870217b5ecdbacd3
Gabriel Tickle <gabriel.cl.tickle@gmail.com>
2021-10-20 02:33:25 -0500

    Made more of the project into Methods Added the ability to get the information entered when logging in

--- Java/OOPproject/mainGUI.java
+++ Java/OOPproject/mainGUI.java
@@ -1,17 +1,18 @@
 package OOPproject;
+
 import OOPproject.adminGui;
 import OOPproject.customerGui;
 
 import java.awt.geom.RoundRectangle2D;
-import java.awt.BorderLayout;
+
 import java.awt.Color;
-import java.awt.FlowLayout;
+
 import java.awt.Font;
-import java.awt.GridLayout; 
+import java.awt.GridLayout;
 import java.awt.event.*;
-import java.awt.Component;
+
 import java.awt.Graphics;
-import java.awt.Insets;
+
 import java.awt.Shape;
 
 import javax.swing.Icon;
@@ -26,15 +27,16 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.JToggleButton;
 import javax.swing.SwingConstants;
-import javax.swing.border.Border;
-
-
 
 public class mainGUI {
 
     final int panelw = 1000;
     final int panelh = 600;
     final int uih = 25;
+    final int admin = 1;
+    final int customer = 0;
+
+    private int panelStatus = 0;
 
     private static JFrame frame;
 
@@ -44,51 +46,42 @@ public class mainGUI {
 
     private static JLabel Welcome;
     private static JLabel picLabel;
-    private static JTextArea adminSwap;
 
-    private static  Icon tech;
-    private static  Icon server;
-
+    private static Icon tech;
+    private static Icon server;
 
     private static JTextField phoneText;
     private static JTextField userText;
+    private static JTextArea adminSwap;
 
     private static JComboBox providerBox;
     private static JPasswordField passwordText;
-    
+
     private static JToggleButton adminButton;
     private static JButton loginButton;
-    
+
     private static JButton exitButton;
 
-    private static String[] providors = {"Digicel", "Flow"};
-    
-    public mainGUI()  {
+    private static String[] providors = { "Digicel", "Flow" };
+
+    public mainGUI() {
 
         tech = new ImageIcon("C:/Users/sptic/Desktop/tech1.png");
         server = new ImageIcon("C:/Users/sptic/Desktop/server1.png");
-        
 
-        frame = new JFrame();
-        basePanel = new JPanel();
+        createFrame();
+
+        basePanel.setLayout(new GridLayout(1, 2));
+
         imagePanel = new JPanel();
         loginPanel = new JPanel();
-        
+
         picLabel = new JLabel(tech);
         picLabel.setBounds(10, 10, 480, 580);
-        
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(null);
-        
-        frame.setBackground(null);
-        frame.add(basePanel);
-        
-        basePanel.setBounds(0, 0, panelw, panelh);
-        basePanel.setLayout(new GridLayout(1,2));
-        
+
         basePanel.add(imagePanel);
         basePanel.add(loginPanel);
-        
+
         imagePanel.setLayout(null);
         loginPanel.setLayout(null);
 
@@ -100,101 +93,121 @@ public class mainGUI {
 
         addLoginButton();
 
-        Welcome = new JLabel("WELCOME!",SwingConstants.CENTER);
+        Welcome = new JLabel("WELCOME!", SwingConstants.CENTER);
         Welcome.setBounds(150, 150, 200, 50);
         Welcome.setForeground(Color.white);
         Welcome.setFont(new Font("Oswald", Font.TYPE1_FONT, 34));
 
         loginPanel.add(Welcome);
-        
+
         imagePanel.add(picLabel);
 
-        imagePanel.setBackground(new Color(250,245,255));
-        loginPanel.setBackground(new Color(120,25,255)); 
+        imagePanel.setBackground(new Color(250, 245, 255));
+        loginPanel.setBackground(new Color(120, 25, 255));
+
+    }
+
+    public void createPanel() {
+
+        basePanel = new JPanel();
+        frame.add(basePanel);
+
+        basePanel.setBounds(0, 0, panelw, panelh);
+        basePanel.setLayout(null);
+
+    }
+
+    public void createFrame() {
+
+        frame = new JFrame();
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(null);
+
+        frame.setBackground(null);
+        createPanel();
 
         frame.setUndecorated(true);
         frame.setShape(new RoundRectangle2D.Double(0, 0, panelw, panelh, 30, 30));
         frame.setSize(panelw, panelh);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
-        
+
     }
 
-    public void addUserLogin(){
+    public void addUserLogin() {
 
         phoneText = new JTextField(25);
         phoneText.setText("User Phone number");
         phoneText.setBounds(125, 250, 250, uih);
-        
-        phoneText.setBorder(javax.swing.BorderFactory.createEmptyBorder());
-        //phoneText.setOpaque(false);
 
-        phoneText.addFocusListener  (new FocusListener () {
+        phoneText.setBorder(javax.swing.BorderFactory.createEmptyBorder());
+        // phoneText.setOpaque(false);
+
+        phoneText.addFocusListener(new FocusListener() {
             @Override
             public void focusGained(FocusEvent e) {
-                // TODO Auto-generated method stub
+
                 phoneText.setText(null);
             }
 
             @Override
             public void focusLost(FocusEvent e) {
-                // TODO Auto-generated method stub
+
                 if (phoneText.getText() == null) {
                     phoneText.setText("User Phone number");
 
                 }
-                
-            } 
-            
-          } );
-        
+
+            }
+
+        });
+
         userText = new JTextField(25);
         userText.setText("User First Name");
         userText.setBounds(125, 310, 250, uih);
-          
+
         userText.setBorder(javax.swing.BorderFactory.createEmptyBorder());
-        //phoneText.setOpaque(false);
-  
-        userText.addFocusListener  (new FocusListener () {
-              @Override
-              public void focusGained(FocusEvent e) {
-                  // TODO Auto-generated method stub
-                  userText.setText(null);
-              }
-  
-              @Override
-              public void focusLost(FocusEvent e) {
-                  // TODO Auto-generated method stub
-                  if (userText.getText() == null) {
+        // phoneText.setOpaque(false);
+
+        userText.addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                // TODO Auto-generated method stub
+                userText.setText(null);
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+                // TODO Auto-generated method stub
+                if (userText.getText() == null) {
                     userText.setText("User First Name");
-  
-                  }
-                  
-              } 
-              
-            } );
-          
+
+                }
+
+            }
+
+        });
+
         passwordText = new JPasswordField("Password");
         passwordText.setBounds(125, 370, 250, uih);
         passwordText.setVisible(true);
-        passwordText.addFocusListener  (new FocusListener () {
-                @Override
-                public void focusGained(FocusEvent e) {
-                    // TODO Auto-generated method stub
-                    passwordText.setText(null);
+        passwordText.addFocusListener(new FocusListener() {
+            @Override
+            public void focusGained(FocusEvent e) {
+                passwordText.setText(null);
+            }
+
+            @Override
+            public void focusLost(FocusEvent e) {
+                if (passwordText.getText() == null) {
+                    passwordText.setText("Password");
                 }
-    
-                @Override
-                public void focusLost(FocusEvent e) {
-                    // TODO Auto-generated method stub
-                    if (passwordText.getText() == null) {
-                        passwordText.setText("Password");
-                    }
-                    
-                } 
-                
-              } );
-    
+
+            }
+
+        });
+
         loginPanel.add(phoneText);
         loginPanel.add(userText);
         loginPanel.add(passwordText);
@@ -206,7 +219,7 @@ public class mainGUI {
     final int adminoffsetX = adminsetX + 5;
     final int adminoffsetY = adminsetY + 9;
 
-    public void addAdminLogin(){
+    public void addAdminLogin() {
 
         adminSwap = new JTextArea("Are You and admin user at either service providor?");
         adminSwap.setEditable(false);
@@ -216,7 +229,7 @@ public class mainGUI {
         adminSwap.setOpaque(false);
         adminSwap.setFont(new Font("Oswald", Font.TYPE1_FONT, 10));
         adminSwap.setForeground(Color.white);
-        adminSwap.setBackground(new Color(0,0,0,0));
+        adminSwap.setBackground(new Color(0, 0, 0, 0));
         adminSwap.setBorder(javax.swing.BorderFactory.createEmptyBorder());
 
         adminButton = new JToggleButton("<HTML><U>Admin</U></HTML>");
@@ -224,17 +237,17 @@ public class mainGUI {
         adminButton.setForeground(Color.white);
         adminButton.setFont(new Font("Oswald", Font.TYPE1_FONT, 10));
 
-        adminButton.addItemListener (new ItemListener() { 
+        adminButton.addItemListener(new ItemListener() {
             @Override
             public void itemStateChanged(ItemEvent itemEvent) {
- 
+
                 adminPressed(itemEvent);
-                
-            } 
-          } );
-        adminButton.setBorderPainted(false); 
-        adminButton.setContentAreaFilled(false); 
-        adminButton.setFocusPainted(false); 
+
+            }
+        });
+        adminButton.setBorderPainted(false);
+        adminButton.setContentAreaFilled(false);
+        adminButton.setFocusPainted(false);
         adminButton.setOpaque(false);
 
         providerBox = new JComboBox(providors);
@@ -249,13 +262,13 @@ public class mainGUI {
 
     }
 
-    public void addExitButton(){
+    public void addExitButton() {
 
         exitButton = new JButton("X");
         exitButton.setForeground(Color.white);
         exitButton.setBounds(455, 0, 45, 45);
-        exitButton.setLayout(new GridLayout(1,1));
-        exitButton.addActionListener(new ActionListener(){
+        exitButton.setLayout(new GridLayout(1, 1));
+        exitButton.addActionListener(new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -263,79 +276,127 @@ public class mainGUI {
             }
 
         });
-        exitButton.setBorderPainted(false); 
-        exitButton.setContentAreaFilled(false); 
-        exitButton.setFocusPainted(false); 
+        exitButton.setBorderPainted(false);
+        exitButton.setContentAreaFilled(false);
+        exitButton.setFocusPainted(false);
         exitButton.setOpaque(false);
         loginPanel.add(exitButton);
 
     }
 
-    public void addLoginButton(){
+    public void addLoginButton() {
 
         loginButton = new JButton("Login");
         loginButton.setBounds(125, 425, 100, uih);
         loginPanel.add(loginButton);
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
 
-    }  
+                if (panelStatus == customer) {
+
+                    String username = userText.getText();
+                    String phone = phoneText.getText();
+                    String password = passwordText.getText();
+
+                    System.out.println("Customer Information\n" + "Username:\t" + username + "\n" + "Password:\t"
+                            + password + "\n"+ "Phone Number:\t" + phone);
+
+                } else if (panelStatus == admin) {
+
+                    int providor = providerBox.getSelectedIndex();
+                    String password = passwordText.getText();
+
+                    switch (providor) {
+                    case 0:
+
+                        System.out.println(
+                                "Admin Information\n" + "Providor:\t" + "Digicel" + "\n" + "Password:\t" + password);
+
+                        break;
+
+                    case 1:
+
+                        System.out.println(
+                                "Admin Information\n" + "Providor:\t" + "Flow" + "\n" + "Password:\t" + password);
+
+                        break;
+
+                    default:
+                        break;
+                    }
+
+                }
+
+            }
+
+        });
+
+    }
 
     protected void adminPressed(ItemEvent itemEvent) {
 
         int state = itemEvent.getStateChange();
-        
+
         if (state == ItemEvent.SELECTED) {
             phoneText.setVisible(false);
             userText.setVisible(false);
             passwordText.setBounds(125, 350, 250, uih);
             providerBox.setVisible(true);
-            
+
             picLabel.setIcon(server);
 
-            //passwordText.setVisible(true);
-            //Welcome.setVisible(false);
+            // passwordText.setVisible(true);
+            // Welcome.setVisible(false);
             Welcome.setText("Admin Panel");
             adminSwap.setText("Are you a Customer at either service providor?");
             adminButton.setText("<HTML><U>User</U></HTML>");
-        }else{
+
+            panelStatus = admin;
+        } else {
             phoneText.setVisible(true);
             userText.setVisible(true);
             passwordText.setBounds(125, 370, 250, uih);
             providerBox.setVisible(false);
 
             picLabel.setIcon(tech);
-            
-            //passwordText.setVisible(false);
-            //Welcome.setVisible(true);
+
+            // passwordText.setVisible(false);
+            // Welcome.setVisible(true);
             Welcome.setText("Welcome!");
             adminButton.setText("<HTML><U>Admin</U></HTML>");
             adminSwap.setText("Are you an Admin user at either service providor?");
+
+            panelStatus = customer;
         }
 
     }
-   
+
     class RoundedJTextField extends JTextField {
         private Shape shape;
+
         public RoundedJTextField(int size) {
-        super(size);
-        setOpaque(false);
-     }
-     protected void paintComponent(Graphics g) {
-        g.setColor(getBackground());
-        g.fillRoundRect(0, 0, getWidth()-1, getHeight()-1, 15, 15);
-        super.paintComponent(g);
-     }
-     protected void paintBorder(Graphics g) {
-        g.setColor(getForeground());
-        g.drawRoundRect(0, 0, getWidth()-1, getHeight()-1, 15, 15);
-     }
-     public boolean contains(int x, int y) {
-        if (shape == null || !shape.getBounds().equals(getBounds())) {
-           shape = new RoundRectangle2D.Float(0, 0, getWidth()-1, getHeight()-1, 15, 15);
+            super(size);
+            setOpaque(false);
         }
-        return shape.contains(x, y);
+
+        protected void paintComponent(Graphics g) {
+            g.setColor(getBackground());
+            g.fillRoundRect(0, 0, getWidth() - 1, getHeight() - 1, 15, 15);
+            super.paintComponent(g);
         }
-     }
+
+        protected void paintBorder(Graphics g) {
+            g.setColor(getForeground());
+            g.drawRoundRect(0, 0, getWidth() - 1, getHeight() - 1, 15, 15);
+        }
+
+        public boolean contains(int x, int y) {
+            if (shape == null || !shape.getBounds().equals(getBounds())) {
+                shape = new RoundRectangle2D.Float(0, 0, getWidth() - 1, getHeight() - 1, 15, 15);
+            }
+            return shape.contains(x, y);
+        }
+    }
 
 }
-
-
